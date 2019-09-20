83383948080bf6befe04362290446c86cace1823
janiiresha <44993229+janiiresha@users.noreply.github.com>
2019-09-20 16:33:42 +0900

    Think Java Chapter 8 Exercises

---
+++ aaa.java
@@ -0,0 +1,19 @@
+
+/**
+ * クラス aaa の注釈をここに書きます.
+ * 
+ * @author (あなたの名前)
+ * @version (バージョン番号もしくは日付)
+ */
+public class aaa
+{
+    public static int banana(int[] a) {
+    int kiwi = 1;
+    int i = 0;
+    while (i < a.length) {
+        kiwi = kiwi * a[i];
+        i++;
+    }
+    return kiwi;
+}
+}
